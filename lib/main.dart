2c89a92267eb0dca60be90ed54debab7120f7bf6
Leonardo Marinho <leonardo.marinho@everi.com>
2022-09-16 00:49:45 -0300

    Vídeo 3.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,21 @@ import 'package:client_control/models/clients.dart';
 import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'models/client_type.dart';
+import 'models/types.dart';
 import 'pages/clients_page.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => Clients(clients: []),
-    child: const MyApp()
-  ));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => Clients(clients: [])),
+    ChangeNotifierProvider(create: (context) => Types(types: [
+      ClientType(name: 'Platinum', icon: Icons.credit_card),
+      ClientType(name: 'Golden', icon: Icons.card_membership),
+      ClientType(name: 'Titanium', icon: Icons.credit_score),
+      ClientType(name: 'Diamond', icon: Icons.diamond),
+    ]))
+  ],
+  child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
