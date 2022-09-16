45bfc9560306584301ad6b6e8445a11f287a8f77
Leonardo Marinho <leonardo.marinho@everi.com>
2022-09-16 00:33:48 -0300

    Vídeo 3.3

---
+++ lib/models/types.dart
@@ -0,0 +1,15 @@
+import 'package:client_control/models/client_type.dart';
+import 'package:flutter/material.dart';
+
+class Types extends ChangeNotifier {
+  List<ClientType> types;
+
+  Types({
+    required this.types
+  });
+
+  void add(ClientType type) {
+    types.add(type);
+    notifyListeners();
+  }
+}
