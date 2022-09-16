ba48411b180da90b0f6027340101d71a444e7f53
Leonardo Marinho <leonardo.marinho@everi.com>
2022-09-15 23:04:05 -0300

    Vídeo 2.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:client_control/models/clients.dart';
 import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'pages/clients_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => Clients(clients: []),
+    child: const MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
